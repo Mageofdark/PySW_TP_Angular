4ba236724df0d1346abb70a46080d718e4632056
Ignacio Huanca <ignacarp06@gmail.com>
2026-05-03 04:09:52 -0300

    modo oscuro añadido

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { ThemeService } from './services/theme-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('TP_Angular');
+
+  // Tema modo oscuro / claro
+  constructor(private themeService: ThemeService) {}
+  
+  ngOnInit() {
+    this.themeService.initTheme();
+  }
+
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
+  get theme(){
+    return this.themeService.currentTheme();
+  }
 }
