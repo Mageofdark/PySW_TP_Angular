976d6e92e122c077c755ee7327e31fe74dc64bef
ApazaIgnacio <nacho10apaza@gmail.com>
2026-05-10 17:39:22 -0300

    Punto 3 by Apaza Ignacio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './components/home/home';
 import { AboutComponent } from './components/about/about';
 import { Punto1Component } from './punto1/punto1';
 import { InscripcionesComponent } from './pages/inscripciones-component/inscripciones-component';
+import { Punto3 } from './punto3/punto3'; //agregado por ignacio apaza
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'punto1', component: Punto1Component },
-  { path: 'inscripciones', component: InscripcionesComponent }
+  { path: 'inscripciones', component: InscripcionesComponent },
+  { path:'punto3', component:Punto3 } //ruta agregada por ignacio apaza
   // Aquí otros pueden agregar más rutas, por ejemplo:
   // { path: 'contact', component: ContactComponent },
 ];
